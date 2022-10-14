2cf5e540b7bb7db15ef16e39c565b15dffc9a2ff
Karlhe2002 <xuh011@ucsd.edu>
2022-10-14 00:40:34 -0700

    Modified

--- ArrayTests.java
+++ ArrayTests.java
@@ -20,8 +20,20 @@ public class ArrayTests {
 	public void testReverseInPlace1() {
     int[] input1 = { 3 , 2 , 4 , 5};
     ArrayExamples.reverseInPlace(input1);
-    assertArrayEquals(new int[]{ 5,4,2,3 }, input1);
+    assertArrayEquals(new int[]{ 5, 4, 2, 3 }, input1);
 	}
+
+  @Test
+  public void testAverage1(){
+    double[] input1 = { 3.0 , 2 , 4.0 , 5.0};
+    assertEquals(4.0, ArrayExamples.averageWithoutLowest(input1),0);
+  }
+
+  @Test
+  public void testAverage2(){
+    double[] input1 = {1.1, 1.1, 2,3, 4};
+    assertEquals(3, ArrayExamples.averageWithoutLowest(input1),0);
+  }
 
 
 }
--- ListTests.java
+++ ListTests.java
@@ -1,1 +1,25 @@
+import static org.junit.Assert.*;
+import java.util.ArrayList;
+import org.junit.*;
 
+class ShortStringChecker implements StringChecker {
+    @Override
+    public boolean checkString(String s) {
+        return (s.length() < 5);
+    }
+}
+
+public class ListTests {
+ @Test 
+ public void shortfilter() {
+    ArrayList<String> input23 = new ArrayList<>();
+    input23.add("sd");
+    input23.add("abf");
+    ArrayList<String> answer = new ArrayList<>();
+    answer.add("sd");
+    answer.add("abf");
+    StringChecker sc = new ShortStringChecker();
+    assertEquals(true, answer.equals(ListExamples.filter(input23, sc)));
+}
+
+}
